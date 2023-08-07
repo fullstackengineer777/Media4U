b4e0376a14429faf039e7445847e9ccf081d69f6
fullstackengineer7 <fullstackdev3546@gmail.com>
2023-08-07 16:23:39 +0900

    first url

--- index.py
+++ index.py
@@ -5,7 +5,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -63,30 +62,64 @@ wait.until(EC.url_to_be(url))
 if get_url == url:
     page_source = driver.page_source
 
-    # soup = BeautifulSoup(page_source, features="html.parser")
-    # keyword = input("Enter a keyword to find: ")
-    # matches = soup.body.find_all(string=re.compile(keyword))
-    # len_match = len(matches)
-    # title = soup.title.text
-    # # print(title)
+    soup = BeautifulSoup(page_source, features="html.parser")
+    groups = soup.find_all('li', {'class': 'tileGrid-tile'})
+    for group in groups:
+        link = group.find("a")["href"]
+        print(link)
+        driver.get(link)
 
-    # file=codecs.open('article_scraping.txt', 'a+')
+        soup = BeautifulSoup(driver.page_source, features="html.parser")
+        ul_elements = soup.select('ul.flex.flex-col.space-y-5')
+        print("Count of ul = ", len(ul_elements))
+        # print(ul_element)
+        for ul_element in ul_elements:
+            li_elements = ul_element.find_all_next('li')
+            print("Count of li = ", len(li_elements))
+            for li_element in li_elements:
+                event_link = li_element.find("a")["href"]
+                print(event_link)
 
-    # file.write(title+"\n")
-    # file.write("The following are all instances of your keyword:\n")
-    # count=1
-    # for i in matches:
-    #     file.write(str(count) + "." + i + "\n")
-    #     count += 1
+        temp = input("Next group ? : ")
+      
 
-    # file.write("There were " + str(len_match) + " matches found for the keyword.")
-    # file.close()
+        # event_uls = soup.select('ul.space-y-5')
+        # print(event_uls)
 
-    # file_page = codecs.open('page.txt', 'w')
-    # page_source_utf8 = page_source.encode('utf-8')
-    # file_page.write(page_source_utf8)
-    # file_page.close()
+        # for event_ul in event_uls:
+        #     events = soup.find_all_next('li')
+        #     print(events)
+        #     for event in events:
+        #         event_link = event.find("a")["href"]
+        #         print(event_link)
+        #         # driver.get(event_link)
+        #         temp_e = input("Next event ? : ")
 
-    print(page_source)
+
+
+
+        # keyword = input("Enter a keyword to find: ")
+        # matches = soup.body.find_all(string=re.compile(keyword))
+        # len_match = len(matches)
+        # title = soup.title.text
+        # # print(title)
+
+        # file=codecs.open('article_scraping.txt', 'a+')
+        # file.write(title+"\n")
+        # file.write("The following are all instances of your keyword:\n")
+        # count=1
+        # for i in matches:
+        #     file.write(str(count) + "." + i + "\n")
+        #     count += 1
+
+        # file.write("There were " + str(len_match) + " matches found for the keyword.")
+        # file.close()
+
+        # file_page = codecs.open('page.txt', 'w')
+        # page_source_utf8 = page_source.encode('utf-8')
+        # file_page.write(page_source_utf8)
+        # file_page.close()
+        # print(page_source)
+
     keyword = input("Enter keyword : ")
     driver.quit()
